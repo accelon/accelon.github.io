d778b8160caebd92175d39ac911bfc6ba494ea89
yapcheahshen <yapcheahshen@gmail.com>
2014-11-01 20:54:15 +0800

    add installer

---
+++ installer/ksana.js
@@ -0,0 +1,44 @@
+jsonp_handler({
+ "version": "1.0",
+ "build": 393,
+ "title": "installer",
+ "date": "2014-11-01T12:46:06.754Z",
+ "baseurl": "http://accelon.github.io/installer/",
+ "description": "installer",
+ "files": [
+  "index.html",
+  "build.js",
+  "build.css",
+  "jquery.js",
+  "package.json",
+  "ksana.js",
+  "react-with-addons.js",
+  "nodemain.js",
+  "systemmenu.js",
+  "banner.png"
+ ],
+ "filedates": [
+  "2014-10-31T03:37:21.000Z",
+  "2014-11-01T09:47:50.000Z",
+  "2014-11-01T09:47:50.000Z",
+  "2014-10-20T15:05:36.000Z",
+  "2014-11-01T05:30:02.000Z",
+  "2014-11-01T10:00:22.000Z",
+  "2014-10-20T15:05:36.000Z",
+  "2014-11-01T09:36:16.000Z",
+  "2014-11-01T09:47:48.000Z",
+  "2014-11-01T06:58:56.000Z"
+ ],
+ "filesizes": [
+  422,
+  123049,
+  123091,
+  277980,
+  400,
+  799,
+  671712,
+  547,
+  1508,
+  59288
+ ]
+})
