edb6b7dd3f55fcce5b8465c8f68d7939f19abc30
yapcheahshen <yapcheahshen@gmail.com>
2015-01-14 20:38:40 +0800

    add online app list

---
+++ applist.js
@@ -0,0 +1,6 @@
+jsonp_handler([
+	{appid:"nanchuan",title:"南傳大藏經",url:"http://ya.ksana.tw/nanchuan/"}
+	,{appid:"cbeta2014",title:"CBETA 2014 大正藏",url:"http://ya.ksana.tw/cbeta/"}
+	,{appid:"mebagidx",title:"引得市 公益版",url:"http://rawgit.com/yapcheahshen/mebagidx/master/"}
+	,{appid:"moedict-yu",title:"點典(教育部國語辭典)",url:"http://rawgit.com/ahsyuyu/moedict-yu/master/"}
+]);
